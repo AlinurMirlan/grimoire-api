88d85c3e61b9786170d4171b280f71e3160dd3c0
Alinur Mirlan <a18nur.m1r1an@gmail.com>
2024-05-13 18:18:26 +0600

    feat: :sparkles: add domain models and update csproj
    Added domain models and .NET DynamoDb SDK

---
+++ Grimoire.Api/Models/Book.cs
@@ -0,0 +1,13 @@
+﻿using Amazon.DynamoDBv2.DataModel;
+
+namespace Grimoire.Api.Models;
+
+public class Book
+{
+    [DynamoDBHashKey]
+    public string Isbn { get; set; } = string.Empty;
+
+    public string Title { get; set; } = string.Empty;
+
+    public string Description { get; set; } = string.Empty;
+}
