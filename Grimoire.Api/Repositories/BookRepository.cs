afe2822d29429d9053d8b2534ef678d2cec30c90
Alinur Mirlan <a18nur.m1r1an@gmail.com>
2024-05-13 18:18:53 +0600

    feat: :wrench: add book repository

---
+++ Grimoire.Api/Repositories/BookRepository.cs
@@ -0,0 +1,44 @@
+﻿using Amazon.DynamoDBv2;
+using Amazon.DynamoDBv2.DataModel;
+using Amazon.DynamoDBv2.DocumentModel;
+using Amazon.DynamoDBv2.Model;
+using Grimoire.Api.Models;
+
+namespace Grimoire.Api.Repositories;
+
+public class BookRepository(
+    IDynamoDBContext DynamoDBContext,
+    IAmazonDynamoDB AmazonDynamoDB) 
+    : IBookRepository
+{
+    public async Task SaveBookAsync(Book book) => await DynamoDBContext.SaveAsync(book);
+
+    public async Task DeleteBookAsync(string isbn) => await DynamoDBContext.DeleteAsync<Book>(isbn);
+
+    public Task<Book> GetBookByIsbnAsync(string isbn) => DynamoDBContext.LoadAsync<Book>(isbn);
+
+    public async Task<PagedResults<Book>> GetBooksAsync(int count, string? lastEvaluatedKey)
+    {
+        var scanRequest = new ScanRequest
+        {
+            Limit = count,
+            TableName = nameof(Book),
+            ExclusiveStartKey = new Dictionary<string, AttributeValue>()
+            {
+                { nameof(Book.Isbn), new AttributeValue { S = lastEvaluatedKey } }
+            }
+        };
+
+        var scanResponse = await AmazonDynamoDB.ScanAsync(scanRequest);
+        var books = scanResponse.Items.Select(
+            item => DynamoDBContext.FromDocument<Book>(
+                Document.FromAttributeMap(item)));
+    
+        lastEvaluatedKey = scanResponse.LastEvaluatedKey[nameof(Book.Isbn)].S;
+        return new PagedResults<Book>()
+        {
+            Items = books,
+            LastEvaluatedKey = lastEvaluatedKey
+        };
+    }
+}
---
+++ Grimoire.Api/Repositories/IBookRepository.cs
@@ -0,0 +1,14 @@
+﻿using Grimoire.Api.Models;
+
+namespace Grimoire.Api.Repositories;
+
+public interface IBookRepository
+{
+    public Task SaveBookAsync(Book book);
+
+    public Task<PagedResults<Book>> GetBooksAsync(int count, string? lastEvaluatedKey);
+
+    public Task<Book> GetBookByIsbnAsync(string isbn);
+
+    public Task DeleteBookAsync(string isbn);
+}
