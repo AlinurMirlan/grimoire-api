c8a1e098662b47dd93c95fa5cc19800d77a30672
Alinur Mirlan <a18nur.m1r1an@gmail.com>
2024-05-15 10:10:16 +0600

    feat: :art: add an edit-book endpoint and spruce up other ones
    Made use of the newly-created book service instead of directly using repository

--- Grimoire.Api/Program.cs
+++ Grimoire.Api/Program.cs
@@ -1,16 +1,17 @@
-using Amazon.DynamoDBv2.DataModel;
 using Amazon.DynamoDBv2;
-using Grimoire.Api.Repositories;
+using Amazon.DynamoDBv2.DataModel;
+using Grimoire.Api.Exceptions;
 using Grimoire.Api.Models;
-using System.Security.AccessControl;
+using Grimoire.Api.Repositories;
+using Grimoire.Api.Services;
 
 var builder = WebApplication.CreateBuilder(args);
-
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 builder.Services.AddSingleton<IDynamoDBContext, DynamoDBContext>();
 builder.Services.AddSingleton<IAmazonDynamoDB, AmazonDynamoDBClient>();
 builder.Services.AddSingleton<IBookRepository, BookRepository>();
+builder.Services.AddSingleton<IBookService, BookService>();
 
 var app = builder.Build();
 
@@ -22,35 +23,61 @@ if (app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 
-app.MapGet("/book/{isbn}", async (IBookRepository bookRepository, string isbn) =>
+app.MapGet("/book/{isbn}", async (IBookService bookService, string isbn) =>
 {
-    var book = await bookRepository.GetBookByIsbnAsync(isbn);
+    var book = await bookService.GetBookByIsbnAsync(isbn);
     return book is null ? Results.NotFound() : Results.Ok(book);
 })
-.WithName("GetBook")
+.WithName("BookGet")
 .WithOpenApi();
 
-app.MapGet("/books", async (IBookRepository bookRepository, int count, string? lastEvaluatedKey) =>
+app.MapGet("/books", async (IBookService bookService, int? count, string? lastEvaluatedKey) =>
 {
-    return await bookRepository.GetBooksAsync(count, lastEvaluatedKey);
+    return await bookService.GetBooksAsync(count, lastEvaluatedKey);
 })
-.WithName("GetBooks")
+.WithName("BooksGet")
 .WithOpenApi();
 
-app.MapPost("/save/book", async (IBookRepository bookRepository, Book book) =>
+app.MapPost("/book/add", async (IBookService bookService, Book book) =>
 {
-    await bookRepository.SaveBookAsync(book);
-    return Results.Created($"/book/{book.Isbn}", book);
+    Book createdBook;
+    try
+    {
+        createdBook = await bookService.AddBookAsync(book);
+    }
+    catch (ConflictException exception)
+    {
+        return Results.Conflict(exception);
+    }
+
+    return Results.Created($"/book/{book.Isbn}", createdBook);
 })
-.WithName("SaveBook")
+.WithName("BookSave")
 .WithOpenApi();
 
-app.MapDelete("/delete/book/{isbn}", async (IBookRepository bookRepository, string isbn) =>
+app.MapDelete("/book/delete/{isbn}", async (IBookService bookService, string isbn) =>
 {
-    await bookRepository.DeleteBookAsync(isbn);
+    await bookService.DeleteBookAsync(isbn);
     return Results.NoContent();
 })
 .WithName("DeleteBook")
+.WithOpenApi();
+
+app.MapPut("/book/edit", async (IBookService bookService, Book book) =>
+{
+    Book editedBook;
+    try
+    {
+        editedBook = await bookService.EditBookAsync(book);
+    }
+    catch (ConflictException exception)
+    {
+        return Results.Conflict(exception);
+    }
+
+    return Results.Ok(editedBook);
+})
+.WithName("BookEdit")
 .WithOpenApi();
 
 app.Run();
